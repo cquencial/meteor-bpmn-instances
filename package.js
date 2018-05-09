0b5e380508a2c01105c6abb21a27b44a74415afc
jankapunkt <jkuester@uni-bremen.de>
2018-05-09 10:38:21 +0200

    added git url

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Keep track of running Bpmn instances from cquencial:bpmn-engine',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/cquencial/meteor-bpmn-instances.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md',
